fce0fc7a87460854cb5fef619ddcb1d4f61b658d
Ali <belitherali@gmail.com>
2024-08-09 08:06:45 +0100

    updated fetch post route to show on user profile

--- controllers/places.js
+++ controllers/places.js
@@ -94,6 +94,18 @@ router.put('/:placeId', async (req, res) => {
   }
 })
 
+// * GET USER'S PLACES
+router.get('/user/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params
+    const userPlaces = await Place.find({ user: userId }).populate('user')
+    return res.json(userPlaces)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: error.message })
+  }
+})
+
 
 // * DELETE
 router.delete('/:placeId', async (req, res) => {
@@ -132,31 +144,31 @@ router.delete('/:placeId', async (req, res) => {
 router.post('/:placeId/comments', async (req, res) => {
   const { placeId } = req.params
   try {
-    // Add user key to req.body based on authenticated user _id
+    
     req.body.user = req.user._id
     
-    // Search for the parent place (that the comment should be added to)
+    
     const place = await Place.findById(placeId)
 
-    // Send 404 if not found
+    
     if (!place){
       res.status(404)
       throw new Error('Place not found!')
     }
 
-    // Add the comment to the comments array
+    
     place.comments.push(req.body)
 
-    // Save the parent document to persist to the database
+    
     await place.save()
 
-    // Find newly added comment (last index of place.comments array)
+    
     const newComment = place.comments[place.comments.length - 1]
 
-    // Add req.user to user field
+    
     newComment._doc.user = req.user
 
-    // Send new comment in 201 response
+    
     return res.status(201).json(newComment)
   } catch (error) {
     console.error(error)
