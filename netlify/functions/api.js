77f3587b956bc492ba9459060b5d10e616b84456
Ali <belitherali@gmail.com>
2024-08-09 11:22:27 +0100

    deployment

---
+++ netlify/functions/api.js
@@ -0,0 +1,33 @@
+
+const serverless = require('serverless-http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+const usersRouter = require('../../controllers/users');
+const profilesRouter = require('../../controllers/profiles');
+const placesRouter = require('../../controllers/places.js');
+
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.use('/users', usersRouter);
+app.use('/profiles', profilesRouter);
+app.use('/places', placesRouter);
+
+
+module.exports.handler = serverless(app);
